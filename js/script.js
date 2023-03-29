1cfcd034da81ec65d469c896609254d781966807
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-03-28 20:39:01 -0400

    added some psuedocode and initial ideas of how to write code in js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var saveButton= $('.saveBtn');
+var timeBlock = $('.time-block');
 
-var currentHour= dayjs().hour();
+saveButton.on('click', function(this){
+
+
+})
+
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -16,11 +24,21 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+
+              // var currentHour= dayjs().hour;
+              // if ('retrieve timeblock ids' < currentHour){
+              //   timeBlock.attr('.past');
+              // } else if ('retrieve ids' > currentHour) {
+              //   timeBlock.attr('.future');
+              // } else {
+              //   timeBlock.attr('.present')
+              // }
+              // how do i retrieve these ids ? should I use attr or another manner?
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
   // TODO: Add code to display the current date in the header of the page.
-
+  
 });
