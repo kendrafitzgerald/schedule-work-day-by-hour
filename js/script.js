260bf1c566dc9b1ab141a6b64c91fc541e29c029
Kendra Fitzgerald <kendrajfitzgerald@gmail.com>
2023-03-31 12:02:28 -0400

    used each function in js to go through each div with class time block, made hourblock variable the number form of each id to match with currentHour

--- js/script.js
+++ js/script.js
@@ -3,34 +3,20 @@ $(function () {
   // These variables utilize dayJS to retrieve the current day and time.
     var currentDay = dayjs();
     var currentHour= dayjs().hour();
-
-    //These variables correspond to the text area and the entire container element of the page
-    var textArea = $(".description");
-    var containerEl = $(".container-lg")
   
-    var containerChildren = containerEl.children();
   
     var storedTasks= [];
-  
-     //These conditional statements state that if hour id in the data-hour attribute divs is less than the current
-    //hour given by dayJS, then the text area will be given a class of past, or grey. If the hourID is greater
-    //than the current hour, it will be given a class of future and will be red. Lastly, if the hour times are
-    //equal, or it is presently that hour, the text area will be given a class of present or red.
-  
-    for (let i = 0; i < containerChildren.length; i++) {
-  
-      var hourBlock = containerChildren[i];
 
-      var hourID = $(hourBlock).data("hour")
+  
+  
+  
+      $(".time-block").each(function() {
 
-      if (hourID < currentHour){
-        textArea.attr("class", "past")
-        } else if (hourID > currentHour) {
-        textArea.attr("class", "future");
-       } else {
-       textArea.attr("class", "present")
-       }
-    }
+        var hourBlock = parseInt($(this).attr("id"));
+
+
+      })
+  
     // This line of code accesses the header, with an ID of current day, and renders the current date on the 
     // page by attributing the text of the dayjs variable.
     $('#currentDay').text(currentDay.format('dddd, MMMM D'));
